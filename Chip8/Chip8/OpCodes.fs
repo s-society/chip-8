068ad9c4ccd9b4940dbc785c0e08426af393c68b
ilnoumri <ilnoumri@yahoo.fr>
2016-10-14 19:58:40 +0200

    Added 00E0 FIXME

--- Chip8/Chip8/OpCodes.fs
+++ Chip8/Chip8/OpCodes.fs
@@ -1,4 +1,14 @@
 ﻿module OpCodes
+open chip8
 
 //35 opcodes
+pixelsvalues = chip8.pixelsvalues
+let opcodes (op) = match op with 
+| 00E0 ->  (let i = 0 in while i < pixelsvalues.Length do
+                pixelsvalues.[i] <- 0us
+                pixelsvalues.[i+1] <- 0us
+                pixelsvalues.[i+2] <- 0us
+                pixelsvalues.[i+3] <- 255us 
+                i = i + 1)
+                chip8.PC = chip8.PC + 2us
 
