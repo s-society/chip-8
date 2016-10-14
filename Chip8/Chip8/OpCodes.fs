f0bbea10b02f79079f53966d764b661a38da25c0
ilnoumri <ilnoumri@yahoo.fr>
2016-10-14 23:50:54 +0200

    FIX 00E0 Instruction

--- Chip8/Chip8/OpCodes.fs
+++ Chip8/Chip8/OpCodes.fs
@@ -2,10 +2,10 @@
 open chip8
 
 //35 opcodes
-screen = chip8.screen
+
 //Instruction pour effacer l'écran 
-let  instruction_00E0 = let i = 0 in  while i < screen.Length do
-                screen.[i] <- 0u 
+let  instruction_00E0 = let i = 0 in  while i < chip8.screen.Length do
+                chip8.screen.[i] <- 0uy
                 i = i + 1 ;
                 chip8.PC = chip8.PC + 2us
 
