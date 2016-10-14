ba869c66461b676cd11361719898a9371ad59a5d
ilnoumri <ilnoumri@yahoo.fr>
2016-10-14 22:46:06 +0200

    Modify 00E0 Instruction

--- Chip8/Chip8/OpCodes.fs
+++ Chip8/Chip8/OpCodes.fs
@@ -3,12 +3,9 @@ open chip8
 
 //35 opcodes
 pixelsvalues = chip8.pixelsvalues
-let opcodes (op) = match op with 
-| 00E0 ->  (let i = 0 in while i < pixelsvalues.Length do
-                pixelsvalues.[i] <- 0us
-                pixelsvalues.[i+1] <- 0us
-                pixelsvalues.[i+2] <- 0us
-                pixelsvalues.[i+3] <- 255us 
-                i = i + 1)
+//Instruction pour effacer l'écran 
+let  instruction_00E0 = let i = 0 in  while i < pixelsvalues.Length do
+                pixelsvalues.[i] <- 0u 
+                i = i + 1 ;
                 chip8.PC = chip8.PC + 2us
 
