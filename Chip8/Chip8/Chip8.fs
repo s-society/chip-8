418fc11560e68da163bad39615a759c212987105
ilnoumri <ilnoumri@yahoo.fr>
2016-10-14 23:11:14 +0200

    Fix screen array

--- Chip8/Chip8/Chip8.fs
+++ Chip8/Chip8/Chip8.fs
@@ -4,8 +4,8 @@ let memory = Array.create 4096 0uy
 let PC = 0x200us
 let Vx = Array.create 16 0uy
 let I = 0us
-// tableau representant la valeur noire = 0 blanc = 1 rdes 2048 pixels (affichage 64*32)
-let pixelsvalues = Array.create 2048 0us
+// tableau representant la valeur noire = 0 blanc = 1 des 2048 pixels (affichage 64*32)
+let sceen = Array.create 2048 0us
 
 let romFile = ""
 
