b82ea5e9e0eb0034ccc3c0d19c974c81ec5d2f26
ilnoumri <ilnoumri@yahoo.fr>
2016-10-15 00:27:53 +0200

    Added mutable PC

--- Chip8/Chip8/Chip8.fs
+++ Chip8/Chip8/Chip8.fs
@@ -1,7 +1,7 @@
 ﻿module chip8
 
 let memory = Array.create 4096 0uy
-let PC = 0x200us
+let mutable PC = 0x200us
 let Vx = Array.create 16 0uy
 let I = 0us
 // tableau representant la valeur R,G,B,A des 2048 pixels (affichage 64*32)
