22261c43e602112ec000594aaec542edcb7ccbd9
Antoine Martin <antoine97.martin@gmail.com>
2016-10-16 16:22:44 +0200

    Make Vx registers mutable
    Closes #49

--- Chip8/Chip8/Chip8.fs
+++ Chip8/Chip8/Chip8.fs
@@ -2,7 +2,7 @@
 
 let memory = Array.create 4096 0uy
 let mutable PC = 0x200us
-let Vx = Array.create 16 0uy
+let mutable Vx = Array.create 16 0uy
 let mutable I = 0us
 // tableau representant la valeur noire = 0 blanc = 1 des 2048 pixels (affichage 64*32)
 let screen = Array.create 2048 0uy
