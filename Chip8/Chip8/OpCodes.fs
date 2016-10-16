8f17cfe90998b88e627441e596f6162019081ffd
Antoine Martin <antoine97.martin@gmail.com>
2016-10-16 16:49:43 +0200

    Add opcode FX1E
    Closes #33
    
    Signed-off-by: Antoine Martin <antoine97.martin@gmail.com>

--- Chip8/Chip8/OpCodes.fs
+++ Chip8/Chip8/OpCodes.fs
@@ -6,8 +6,8 @@
 let  instruction_00E0 = let mutable i = 0 in while i < chip8.screen.Length do
                                                 chip8.screen.[i] <- 0uy
                                                 i <- i + 1 ;
-                        chip8.PC <- chip8.PC + 2us           
-                                     
+                        chip8.PC <- chip8.PC + 2us
+
 // 3XKK - Passe à l'instruction suivante si VX = KK
 let instruction_3XKK opcode = let X = int (opcode &&& 0x0F00us >>> 8) in //garde seulement la première valeur
                               let KK = byte (opcode &&& 0x00FFus) in
@@ -23,11 +23,11 @@ let instruction_6XKK opcode = let X = int (opcode &&& 0x0F00us >>> 8) in //garde
                               let KK = byte (opcode &&& 0x00FFus) in
                               chip8.Vx.[X] <- KK
                               chip8.PC <- chip8.PC + 2us
-                              
+
 // 7XKK - Ajoute KK à VX
 let instruction_7XKK opcode = let X = int (opcode &&& 0x0F00us >>> 8) in //garde seulement la première valeur
-                              let KK = byte (opcode &&& 0x00FFus) in    
-                              chip8.Vx.[X] <- chip8.Vx.[X] + KK 
+                              let KK = byte (opcode &&& 0x00FFus) in
+                              chip8.Vx.[X] <- chip8.Vx.[X] + KK
                               chip8.PC <- chip8.PC + 2us
 
 // 9XY0 - Passe à l'instruction suivante si VX != VY
@@ -37,6 +37,10 @@ let instruction_9XY0 opcode = let X = int ((opcode &&& 0x0F00us) >>> 8) in
 
 // ANNN - Affecter NNN à I
 let instruction_ANNN opcode = chip8.I <- ( opcode &&& 0x0FFFus) // garde seulement les 3 dernières valeurs pour les assigner
+                              chip8.PC <- chip8.PC + 2us
+
+// FX1E - Affecte VX + I à I
+let instruction_FX1E opcode = chip8.I <- chip8.I + uint16 chip8.Vx.[int ((opcode &&& 0x0F00) >>> 8)]
                               chip8.PC <- chip8.PC + 2us
 
 // FX65 - Stock les valeurs de de la mémoire à l'adresse I jusqu'à I + X dans VO jusqu'à VX
