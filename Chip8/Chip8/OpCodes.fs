f925b9cc5517381c016195719233320684608634
MARTIN <mart1meunier@gmail.com>
2016-10-25 16:43:40 +0200

    Update OpCodes.fs

--- Chip8/Chip8/OpCodes.fs
+++ Chip8/Chip8/OpCodes.fs
@@ -43,6 +43,7 @@ let instruction_ANNN opcode = chip8.I <- ( opcode &&& 0x0FFFus) // garde seuleme
 let insttuction_FX07 opcode = let X = int ((opcode &&& 0x0F00us) >>>8) in
                               Vx.[X] <- chip8.DelayTimer
                               chip8.PC <- chip8.PC + 2us
+                              
 // FX1E - Affecte VX + I à I
 let instruction_FX1E opcode = chip8.I <- chip8.I + uint16 chip8.Vx.[int ((opcode &&& 0x0F00us) >>> 8)]
                               chip8.PC <- chip8.PC + 2us
