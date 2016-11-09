5f3af43a1d67588fc4a488959ec4a8178106957e
Antoine <antoine97.martin@gmail.com>
2016-11-09 16:51:04 +0100

    Remove useless comments at the top of Program.fs
    Signed-off-by: Antoine <antoine97.martin@gmail.com>

--- Chip8/Chip8/Program.fs
+++ Chip8/Chip8/Program.fs
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-open System
+﻿open System
 open System.Windows.Forms
 open System.IO
 open System.Drawing
